0b9b654d83e0e384cb9eb67244fab2eea9bc8959
Coding Jerk <codingjerk@gmail.com>
2018-09-27 14:05:19 +0300

    Stylefixes in autocolor

--- autocolor.py
+++ autocolor.py
@@ -4,11 +4,7 @@ description = 'Automaticly runs program with --color=auto key if needed'
 def match(args):
     cmd_name = args[0] if len(args) else None
 
-    if cmd_name == 'ls':
-        return True
-        
-    return False
+    return cmd_name == 'ls'
 
 def modify(args):
     return args[:1] + ['--color=auto'] + args[1:]
-
